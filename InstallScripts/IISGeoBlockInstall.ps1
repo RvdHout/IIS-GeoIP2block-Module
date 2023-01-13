ee11845c3ceb126abbdfe756de7bfe5a61305a59
Artur Lebedev <ras_atari@mail.ru>
2023-01-13 14:20:12 +0300

    Update IISGeoBlockInstall.ps1
    Fixing quotes

--- InstallScripts/IISGeoBlockInstall.ps1
+++ InstallScripts/IISGeoBlockInstall.ps1
@@ -3,7 +3,7 @@ $InstallPath=$args[1]
 Invoke-WebRequest https://github.com/6eh01der/IIS-GeoIP2block-Module/releases/download/$ReleaseVersion/IIS-GeoIP2block-Module-$ReleaseVersion.zip -OutFile $env:windir\Temp\IIS-GeoIP2block-Module-2.4.1.0.zip
 Invoke-WebRequest https://github.com/6eh01der/IIS-GeoIP2block-Module/raw/master/InstallScripts/IISManagerGeoBlockReg.vbs -OutFile $env:windir\Temp\IISManagerGeoBlockReg.vbs
 Expand-Archive -LiteralPath $env:windir\Temp\IIS-GeoIP2block-Module-$ReleaseVersion.zip -DestinationPath "$env:windir\Temp\IIS-GeoIP2block-Module-$ReleaseVersion\"
-Move-Item "$env:windir\Temp\IIS-GeoIP2block-Module-$ReleaseVersion\release\geoblockModule_schema.xml" '$env:windir\System32\inetsrv\config\schema\' -Force
+Move-Item "$env:windir\Temp\IIS-GeoIP2block-Module-$ReleaseVersion\release\geoblockModule_schema.xml" "$env:windir\System32\inetsrv\config\schema\" -Force
 New-Item -ItemType Directory "$InstallPath\IISGeoIP2blockModule" -Force
 Move-Item "$env:windir\Temp\IIS-GeoIP2block-Module-$ReleaseVersion\release\*" "$InstallPath\IISGeoIP2blockModule\" -Force
 [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")            
